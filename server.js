98b374cacfdaa16580599c50df3469ceb4a92c89
mariabreuv <17maria.abreu@gmail.com>
2024-12-19 16:19:08 +0000

    Loading add note section dynamically

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const path = require('path');
 const app = express();
 const client = new mongodb.MongoClient('mongodb://localhost:27017');
 
-// Startup
 async function main() {
     process.on('SIGINT', async () => {
         await client.close();
@@ -14,18 +13,15 @@ async function main() {
 
     await client.connect();
 
-    // Serve static files
     app.use('/static', express.static(path.resolve(__dirname, 'static')));
     app.use(express.json())
 
-    // Endpoints
     app.post('/api/login', loginUser);
     app.get('/api/notes', getAllNotes);
     app.post('/api/notes', addNote);
     app.put('/api/notes/:id', updateNote);
     app.delete('/api/notes/:id', deleteNote);
 
-    // Serve the frontend (index.html)
     app.get('/*', (message, response) => {
         response.sendFile(path.resolve(__dirname, 'index.html'));
     });
@@ -33,7 +29,6 @@ async function main() {
     app.listen(5500, () => console.log('Server running on port'));
 }
 
-// Handlers
 async function loginUser(message, response) {
     const { username, password } = message.body;
     const owners = client.db('finalProject').collection('owner'); // Changed to 'owner'
@@ -55,22 +50,20 @@ async function loginUser(message, response) {
     }
 }
 
-
+//see this again
 async function getAllNotes(message, response) {
     const db = client.db('finalProject');
-    const owner = message.query.owner; // Get the owner username from query parameters
+    const owner = message.query.owner;
 
-    // Find the owner's ObjectId
+    
     const ownerData = await db.collection('owner').findOne({ username: owner });
     if (!ownerData) {
         response.status(404).json({ error: 'Owner not found.' });
         return;
     }
 
-    // Fetch notes for this owner
     const notes = await db.collection('notes').find({ owner: ownerData._id }).toArray();
 
-    // Format the notes
     const formattedNotes = notes.map(note => ({
         id: note._id.toString(),
         title: note.title,
